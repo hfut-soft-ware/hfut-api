4d6ad22edba9f89017be85d405115ce975a5d193
snowingfox <1503401882@qq.com>
2022-04-07 22:48:47 +0800

    :sparkles: feat: app an GET server

--- app.ts
+++ app.ts
@@ -1,8 +1,29 @@
 import express from 'express'
 import config from './src/config/config'
+import { login } from './src/modules/login'
 
 const app = express()
 
 app.listen(config.server.port, () => {
   console.log(`Server is running on port ${config.server.port}`)
+})
+
+app.get('/login', async(req, res) => {
+  const query = req.query
+  const result = await login({ username: query.username as string, password: query.password as string })
+
+  if (!result) {
+    res.send({
+      code: -1,
+      msg: '账号或密码错误',
+    })
+
+    return
+  }
+
+  res.send({
+    code: 1,
+    msg: '登录成功',
+    data: result,
+  })
 })
