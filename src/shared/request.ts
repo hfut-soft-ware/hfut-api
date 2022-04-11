99ef56b84965df3cc75479ac9d61ef7719916108
snowingfox <1503401882@qq.com>
2022-04-11 16:17:22 +0800

    :sparkles: feat: use closure

--- src/shared/request.ts
+++ src/shared/request.ts
@@ -13,20 +13,29 @@ export function getCookie(cookie: string | string[]): string {
   return cookie[0]
 }
 
-const headers = { cookie: '' }
+export function createRequest() {
+  let headers = { cookie: '' }
 
-export default function request(options: IRequest): AxiosPromise {
-  return new Promise((resolve, reject) => {
-    if (options.cookie) {
-      headers.cookie = options.cookie
-    }
+  return (options: IRequest, clearHeaders = false): AxiosPromise => {
+    return new Promise((resolve, reject) => {
+      if (clearHeaders) {
+        headers = { cookie: '' }
+      }
+      if (options.cookie) {
+        headers.cookie = options.cookie
+      }
 
-    const config = { method: 'get', headers, maxRedirects: 0, ...(options.config) } as AxiosRequestConfig
+      const config = { method: 'get', headers, maxRedirects: 0, ...(options.config) } as AxiosRequestConfig
 
-    axios(config).then((res) => {
-      resolve(res)
-    }).catch((err) => {
-      reject(err)
+      axios(config).then((res) => {
+        resolve(res)
+      }).catch((err) => {
+        reject(err)
+      })
     })
-  })
+  }
 }
+
+const request = createRequest()
+
+export default request
