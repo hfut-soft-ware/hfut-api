f3a8b50ab2a083f971c3df48ab432bccd0371324
snowingfox <1503401882@qq.com>
2022-04-11 12:11:40 +0800

    :fire: test: add sample

--- src/__test__/server.spec.ts
+++ src/__test__/server.spec.ts
@@ -6,4 +6,10 @@ describe('getRoute', () => {
     const route = getRoute('get_student_info')
     expect(route).toEqual(routeSpy)
   })
+
+  it('should be equal as {/login}', () => {
+    const routeSpy = '/login'
+    const route = getRoute('login')
+    expect(route).toEqual(routeSpy)
+  })
 })
