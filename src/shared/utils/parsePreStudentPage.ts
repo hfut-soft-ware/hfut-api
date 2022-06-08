e8015eb4209550d1ff8d33791e3f1f7d660858aa
snowingfox <1503401882@qq.com>
2022-06-08 10:22:49 +0800

    :bug: 修复预科生数据异常

--- src/shared/utils/parsePreStudentPage.ts
+++ src/shared/utils/parsePreStudentPage.ts
@@ -3,7 +3,20 @@ import * as cheerio from 'cheerio'
 export function parsePreStudentPage(html: string): string {
   const $ = cheerio.load(html)
 
-  const currentCode = $('.btn-info').toArray()[0].attribs.value as string
+  const infoPages = $('.info-page')
+
+  let undergraduateIdx = 0
+
+  // 确保拿到的是预科生本科阶段的数据
+  infoPages.toArray().forEach((page, index) => {
+    const studentType = $(page).children('dl').children('dd').eq(4).text()
+
+    if (studentType.includes('本科')) {
+      undergraduateIdx = index
+    }
+  })
+
+  const currentCode = $('.btn-info').toArray()[undergraduateIdx].attribs.value as string
 
   return currentCode
 }
