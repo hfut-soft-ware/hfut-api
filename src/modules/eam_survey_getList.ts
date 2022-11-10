42fb485afd110f0855669a50e9ef278d0eecf870
yixiaojiu <1918418506@qq.com>
2022-11-10 19:19:55 +0800

    feat: :sparkles: create getSurveyList api

---
+++ src/modules/eam_survey_getList.ts
@@ -0,0 +1,68 @@
+import { load } from 'cheerio'
+import { AxiosError } from 'axios'
+import { IQuery } from '../server'
+import request from '../shared/request'
+
+// lessonSurveyTasks
+interface SurveyTask {
+  teacherName: string
+  id: number
+}
+
+interface List {
+  courseName: string
+  surveyTasks: SurveyTask[]
+}
+
+const baseUrl = 'http://jxglstu.hfut.edu.cn'
+
+export default async function(query: IQuery) {
+  let locatonPath = ''
+  try {
+    await request(`${baseUrl}/eams5-student/for-std/lesson-survey`, {}, query)
+  } catch (error) {
+    locatonPath = (error as AxiosError).response!.headers.location
+  }
+  const res0 = await request(baseUrl + locatonPath, {}, query)
+  const semesterValue = parseHTML(res0.body)
+
+  const res1 = await request(`${baseUrl}/eams5-student/for-std/lesson-survey/${semesterValue}/search/${getStudentId(locatonPath)}`, {}, query)
+
+  const surveyItems = res1.body.forStdLessonSurveySearchVms as any[]
+  const list: List[] = []
+  surveyItems.forEach((surveyItem) => {
+    const lessonSurveyTasks = surveyItem.lessonSurveyTasks as any[]
+    const tasks: SurveyTask[] = []
+
+    lessonSurveyTasks.forEach((taskItem) => {
+      tasks.push({
+        id: taskItem.id,
+        teacherName: taskItem.teacher.person.nameZh,
+      })
+    })
+
+    list.push({
+      courseName: surveyItem.course.nameZh,
+      surveyTasks: tasks,
+    })
+  })
+
+  return {
+    code: 200,
+    msg: '获取获取待教评列表成功',
+    data: {
+      list,
+    },
+  }
+}
+
+function parseHTML(html: string) {
+  const $ = load(html)
+  const optionElement = $('option[selected]')
+  return optionElement.attr('value')
+}
+
+function getStudentId(locatonPath: string) {
+  const items = locatonPath.split('/')
+  return items[items.length - 1]
+}
