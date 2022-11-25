0701a6c13d2e6de364f6797d2ac67e34b07096d3
yixiaojiu <1918418506@qq.com>
2022-11-25 20:34:48 +0800

    feat: :bug: fix survey submit

--- src/modules/eam_survey_submit.post.ts
+++ src/modules/eam_survey_submit.post.ts
@@ -1,13 +1,21 @@
-import { AxiosError } from 'axios'
+import axios, { AxiosError } from 'axios'
 import { IQuery } from '../server'
 import request from '../shared/request'
 
+const url1 = 'http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey'
+
 export default async function(query: IQuery) {
   const data = query.req.body
-  const { cookie } = await request(`http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/start-survey/${data.lessonSurveyTaskAssoc}?REDIRECT_URL=%2Ffor-std%2Flesson-survey%2Fsemester-index%2F${data.studentId}`, {}, query)
+  const studentId = data.studentId
   delete data.studentId
+  try {
+    await request(url1, { maxRedirects: 5, method: 'get' }, { cookie: query.cookie })
+  } catch {}
+  await request('http://localhost:8082/eam/survey/getList', {}, query)
+  const { cookie } = await request(`http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/start-survey/${data.lessonSurveyTaskAssoc}?REDIRECT_URL=%2Ffor-std%2Flesson-survey%2Fsemester-index%2F${studentId}`, {}, query)
 
-  query.cookie = `${query.cookie}; ${cookie![0].split(';')[0]}`
+  console.log(cookie)
+  query.cookie = `${query.cookie}; ${cookie![0].split(';')[0]}; ${cookie![1].split(';')[0]}`
 
   const res = await request('http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/check-can-submit', {
     method: 'POST',
@@ -23,18 +31,27 @@ export default async function(query: IQuery) {
   }
 
   try {
-    const { body } = await request('http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/submit-survey', {
-      method: 'POST',
-      data,
+    const res = await axios({
+      url: 'http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/submit-survey',
       headers: {
-        cookie: query.cookie,
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6,en-GB;q=0.5,hi;q=0.4',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'pragma': 'no-cache',
+        'proxy-connection': 'keep-alive',
+        'cookie': query.cookie,
+        'x-requested-with': 'XMLHttpRequest',
+        'Referer': `http://jxglstu.hfut.edu.cn/eams5-student/for-std/lesson-survey/start-survey/${data.lessonSurveyTaskAssoc}?REDIRECT_URL=%2Ffor-std%2Flesson-survey%2Fsemester-index%2F${studentId}`,
+        'Referrer-Policy': 'strict-origin-when-cross-origin',
       },
-      maxRedirects: 5,
-    }, query)
+      data,
+      method: 'POST',
+    })
     return {
       code: 200,
       msg: '提交成功',
-      data: body,
+      data: res.data,
     }
   } catch (error) {
     return {
--- src/modules/vpn/eam_survey_submit.post.ts
+++ src/modules/vpn/eam_survey_submit.post.ts
@@ -5,9 +5,26 @@ import request from '../../shared/request'
 export default async function(query: IQuery) {
   const data = query.req.body
 
+  const refer = `https://webvpn.hfut.edu.cn/http/77726476706e69737468656265737421faef469034247d1e760e9cb8d6502720ede479/eams5-student/for-std/lesson-survey/start-survey/${data.lessonSurveyTaskAssoc}?REDIRECT_URL=%2Ffor-std%2Flesson-survey%2Fsemester-index%2F${data.studentId}`
   const res = await request('https://webvpn.hfut.edu.cn/http/77726476706e69737468656265737421faef469034247d1e760e9cb8d6502720ede479/eams5-student/for-std/lesson-survey/check-can-submit?vpn-12-o1-jxglstu.hfut.edu.cn', {
     method: 'POST',
     data,
+    headers: {
+      'accept': 'application/json, text/javascript, */*; q=0.01',
+      'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ja;q=0.5,hi;q=0.4',
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+      'pragma': 'no-cache',
+      'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'x-requested-with': 'XMLHttpRequest',
+      'Referer': refer,
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+    },
   }, query)
   const validateResult = res.body.validateResult
   if (!validateResult.passed) {
@@ -19,11 +36,26 @@ export default async function(query: IQuery) {
   }
 
   try {
+    query.cookie = `show_vpn=1; show_faq=0; ${query.cookie}; refresh=1`
     const { body } = await request('https://webvpn.hfut.edu.cn/http/77726476706e69737468656265737421faef469034247d1e760e9cb8d6502720ede479/eams5-student/for-std/lesson-survey/submit-survey?vpn-12-o1-jxglstu.hfut.edu.cn', {
       method: 'POST',
       data,
       headers: {
-        cookie: query.cookie,
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ja;q=0.5,hi;q=0.4',
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        'pragma': 'no-cache',
+        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'origin': 'https://webvpn.hfut.edu.cn',
+        'x-requested-with': 'XMLHttpRequest',
+        'referer': refer,
+        'cookie': query.cookie,
       },
     }, query)
     return {
